18437e0e219bd626b1858143ee0381c2d9e49b51
Anissa Nishioka <ani.nishioka@gmail.com>
2024-04-16 14:54:57 -0700

    post route to /images

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import boto3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+from PIL import Image, ExifTags
+from werkzeug.utils import secure_filename
 
 env = load_dotenv()
 
@@ -20,12 +22,29 @@ BUCKET_NAME = os.environ.get("aws_bucket_name")
 
 @app.get("/images")
 def get_all_images():
+    """Get all image urls."""
+
     public_urls = []
     try:
         for item in s3.list_objects(Bucket=BUCKET_NAME)['Contents']:
-            presigned_url = s3.generate_presigned_url(
-                'get_object', Params={'Bucket': BUCKET_NAME, 'Key': item['Key']}, ExpiresIn=100)
-            public_urls.append(presigned_url)
+            url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{item['Key']}"
+            public_urls.append(url)
     except Exception as e:
-        pass
+        print(e)
     return public_urls
+
+
+@app.post("/images")
+def upload_images():
+    """Upload image to cloud server."""
+
+    image_data = request.files['image']
+    file_name = secure_filename(image_data.filename)
+
+    try:
+        s3.upload_fileobj(image_data, BUCKET_NAME, file_name, ExtraArgs={
+                          'ContentType': image_data.content_type})
+        pass
+    except Exception as e:
+        print(e)
+    return {'success': 'uploaded image', 'fileName': file_name}
